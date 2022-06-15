6dec8b69cdb19f4fb3f18db0ee72b37952a1fed0
egemenelz <egemenelz@gmail.com>
2022-06-15 13:57:05 -0400

    WIP

---
+++ assets/questions.js
@@ -0,0 +1,15 @@
+var questions = [
+    {
+        question: "Inside which HTML element do we put the JavaScript?",
+    },
+    {
+        question: "What is the correct JavaScript syntax to change the content of the HTML element below?                           <p id=demo>This is a demonstration.</p>",
+    },
+    {
+
+    },
+    {
+
+    }
+
+]
